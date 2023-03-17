3ea2d2bc5501a07ec37f21494d2c9e3d3f01c58c
adamrexo <127039714+adamrexo@users.noreply.github.com>
2023-03-17 20:03:22 +0100

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function updateAge() {
+    const birthday = new Date(2007, 3, 18);
+    const ageInMilliseconds = Date.now() - birthday.getTime();
+    const ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+    const age = ageInYears.toFixed(10);
+
+    document.getElementById("age").textContent = age;
+}
+
+updateAge();
+setInterval(updateAge, 10);
