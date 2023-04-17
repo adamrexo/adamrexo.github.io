dd263631035889e082ef2b42dbf130ec59a38752
adamrexo <127039714+adamrexo@users.noreply.github.com>
2023-04-17 14:37:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+  
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+function updateAge() {
+    const birthday = new Date(2007, 3, 18);
+    const ageInMilliseconds = Date.now() - birthday.getTime();
+    const ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+    const age = ageInYears.toFixed(10);
+
+    document.getElementById("age").textContent = age;
+}
+
+updateAge();
+setInterval(updateAge, 10);
